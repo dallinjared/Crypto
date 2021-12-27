3e21cfe574799bafffe26cf3b2906d3afb3461bd
dallinjared <djared@avolta.us>
2021-12-27 15:09:28 -0700

    Coin Detail View with Statistics

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     @EnvironmentObject private var vm : HomeViewModel
     @State private var showPortfolio: Bool = false // Animate right
     @State private var showPortfolioView: Bool = false // New Sheet
+   
+    @State private var selectedCoin: Coin? = nil
+    @State private var showDetailView: Bool = false
+
+    
     var body: some View {
         ZStack {
             // Background Layer
@@ -42,6 +48,11 @@ struct HomeView: View {
                 Spacer(minLength: 0)
             }
         }
+        .background(
+            NavigationLink(isActive: $showDetailView, destination: {
+                DetailLoadingView(coin: $selectedCoin)
+            }, label: { EmptyView() })
+        )
     }
 }
 
@@ -92,6 +103,9 @@ extension HomeView {
             ForEach(vm.allCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: false)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
@@ -107,12 +121,20 @@ extension HomeView {
             ForEach(vm.portfolioCoins) { coin in
                 CoinRowView(coin: coin, showHoldingsColumn: true)
                     .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
             }
         }
         .listStyle(PlainListStyle())
         .refreshable {
             vm.reloadData()
         }
+    }
+    
+    private func segue(coin: Coin) {
+        selectedCoin = coin
+        showDetailView.toggle()
     }
     
     private var columnTitles: some View {
